9acd34a87d2fe49648c8c5675fc46712bbdd89e7
Arjay Jones <robert.jones@arjaycodes.com>
2019-07-13 16:43:13 -0700

    created a SongTimer object for tracking a songs total length (analog to client-side SongTimer without methods)

---
+++ api/util/SongTimer.js
@@ -0,0 +1,12 @@
+const SongTimer = (minutes, seconds, milliseconds) => {
+
+  // Priv.
+  this._minutes      = minutes
+  this._seconds      = seconds
+  this._milliseconds = milliseconds
+
+  // Publ.
+  this.length = `${this._minutes}:${this._seconds}`
+}
+
+export default SongTimer
