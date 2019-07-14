7a63bbc1289141e6e525a814487b360996c17a6a
Arjay Jones <robert.jones@arjaycodes.com>
2019-07-13 22:04:21 -0700

    Cleaned up readability on Song data-model

--- api/db/models/dj/Song.js
+++ api/db/models/dj/Song.js
@@ -4,9 +4,10 @@ const SongTimer = require('../../../util/SongTimer')
 /* THE BASIC DATA NEEDED FOR ACCURATELY TRACKING AND IDENTIFYING INDIVIDUAL SONGS IN A COLLECTION */
 module.exports = mongoose.model('songs', new mongoose
 .Schema({
-	songID:      { type: String }, // The Song's GUID
-	songName:    { type: String },
-  artistName:  { type: String },
-  albumArtURI: { type: String },
-  length:      { type: String, default: new SongTimer(0, 0, 0).length } // A String correlating to SongTimer.length
-}))
+    songID:      { type: String }, // The Song's GUID
+    songName:    { type: String },
+    artistName:  { type: String },
+    albumArtURI: { type: String },
+    length:      { type: String, default: new SongTimer(0, 0, 0).length } // A String correlating to SongTimer.length
+  }))
+  
