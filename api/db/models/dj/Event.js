d663c6c687c254d122fb898bf1c7b0ea52d44f87
Arjay Jones <robert.jones@arjaycodes.com>
2019-07-13 17:34:43 -0700

    Added an Events data-model

---
+++ api/db/models/dj/Event.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+// Supporting Models
+const SongTimer  = require('../../../util/SongTimer')
+
+/* THE DJ OBJECT WHICH TRACKS A DJ'S COLLECTION, PLAYLISTS (IF ANY), AND CURRENT/PAST EVENTS */
+module.exports = mongoose.model('events', new mongoose
+.Schema({
+  name:          { type: String },                                     // The Event's name
+  creator:       { type: String },                                     // The DJ who created this Event
+  locationName:  { type: String },                                     // The name of Venue hosting the Event
+  eventDuration: { type: String, default: new SongTimer(30, 00, 00) }, // The Event's total duration in minutes
+
+  // GUID ref. to optional Playlists. If this is empty, controller should return DJ's collection instead
+  songList:      { type: [String] }
+}))
