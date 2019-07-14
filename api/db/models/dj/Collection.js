dfda33c1669bde7e4a1671d2bf5daf4396998f79
Arjay Jones <robert.jones@arjaycodes.com>
2019-07-13 17:32:48 -0700

    Added a collection ID property and updated the Mongo-Collection name to "songBooks"

--- api/db/models/dj/Collection.js
+++ api/db/models/dj/Collection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
 /* A COLLECTION IS A ONE-TO-ONE LIST RELATED TO A DJ THAT STORES REFERENCES TO EVERY SONG THAT A DJ HAS ON OFFER */
-module.exports = mongoose.model('users', new mongoose
+module.exports = mongoose.model('songBooks', new mongoose
 .Schema({
 	ownerID:        { type: String   }, // The DJ's GUID
+	collectionID:   { type: String   }, // The Collection's GUID
 	artistsIndex:   { type: [String] }, // A non-linked list of Artist names in the DJ's collection
 	songCollection: { type: [Any]    }
 }))
